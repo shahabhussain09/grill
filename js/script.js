5e2570730620d73e7aec3b5c1112090429e60a90
shahabhussain09 <shahabkhan0981233@gmail.com>
2023-08-09 11:29:17 +0500

    comit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick(()=>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+menu.onClick(() => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+// menu.classList.toggle('fa-times');
+// navbar.classList.toggle('active');
 
-})
-    // menu.classList.toggle('fa-times');
-    // navbar.classList.toggle('active');
-
-
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-} 
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
